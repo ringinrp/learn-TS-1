85b9525f20eded39833baecd4293ea4d4070f2fb
ringinrp <ringinrp21@gmail.com>
2024-03-16 18:00:59 +0700

    update function

---
+++ bundle/function.js
@@ -0,0 +1,20 @@
+"use strict";
+function create() {
+    return 2;
+}
+//arrow function
+const create2 = () => "hello world";
+function add(x, y) {
+    const z = x + y;
+    console.log("hasil adalah " + z);
+}
+let ringin;
+ringin = "adalah pria";
+let sayaManusia;
+sayaManusia = "Ringin Restu Pati";
+console.log(create());
+const result = create2();
+console.log(result);
+add(10, 20);
+console.log(ringin);
+console.log(sayaManusia);
---
+++ src/function.ts
@@ -0,0 +1,33 @@
+function create(): number {
+    return 2;
+}
+
+//arrow function
+const  create2 = (): string => "hello world"
+
+function add(x: number , y: number){
+    const z: number = x+y;
+    console.log("hasil adalah " + z);
+}
+
+type Pria = string;
+type Wanita = string;
+
+type Gender = Pria | Wanita;
+
+let ringin: Pria;
+ringin = "adalah pria"
+
+let sayaManusia : Gender;
+sayaManusia ="Ringin Restu Pati"
+
+
+console.log(create());
+
+const result = create2();
+console.log(result);
+
+add(10,20);
+
+console.log(ringin);
+console.log(sayaManusia);
