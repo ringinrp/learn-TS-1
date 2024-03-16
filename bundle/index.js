d54e7ffa715892a944c39ce5cf391ffd0c5d6b11
ringinrp <ringinrp21@gmail.com>
2024-03-16 08:58:14 +0700

    array

---
+++ bundle/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let nama = "ringin restu";
+//array biasa
+let orang;
+orang = ["doni", "tono"];
+//tuple array
+let orang2;
+orang2 = ["joni", "darma"];
+console.log({ nama });
+console.log({ orang });
+console.log({ orang2 });
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let nama: string = "ringin restu";
+
+//array biasa
+let orang: string[];
+orang = ["doni", "tono"];
+
+//tuple array
+let orang2: [string, string];
+orang2 = ["joni", "darma"];
+
+console.log({nama});
+console.log({orang});
+console.log({orang2});
