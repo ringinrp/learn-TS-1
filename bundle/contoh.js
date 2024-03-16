0a0830e91cb2003c423f6d0236ddfc80eac5e5d2
ringinrp <ringinrp21@gmail.com>
2024-03-16 09:20:07 +0700

    tipe data

---
+++ bundle/contoh.js
@@ -0,0 +1,7 @@
+"use strict";
+let temanKita;
+temanKita = {
+    nama: "Restu",
+    status: true
+};
+console.log({ temanKita });
---
+++ src/contoh.ts
@@ -0,0 +1,14 @@
+type Teman = {
+    nama: string;
+    status: boolean;
+    hutang?: number;
+}
+let temanKita: Teman;
+
+temanKita = {
+    nama : "Restu",
+    status: true
+}
+
+console.log({temanKita});
+
